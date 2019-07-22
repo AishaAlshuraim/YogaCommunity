7cfd6efcac34c82b034cf7f66b822d078c31a1a0
aisha <aishaalshuraim@gmail.com>
2019-07-22 16:42:22 +0300

    done with comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,4 +3,11 @@ class CommentsController < ApplicationController
         @video = Video.find(params[:video_id])
         @comment = Comment.new
       end
+
+def create
+    puts params
+    video = Video.find(params[:comment][:video_id])
+    Comment.create(params.require(:comment).permit(:content)
+    redirect_to video
+  end
 end
